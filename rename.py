f3ad217acd4a20cb235963648c71d58bddf5d8f8
Nathan Glasgow <ngglasgow@gmail.com>
2020-01-09 17:20:02 -0500

    added directory walking script

--- rename.py
+++ rename.py
@@ -1,6 +1,5 @@
 import os
 
-cwd = os.getcwd()
 image_dir = '191014VJ_C448L_4_1_gfp_1/'
 file_list = os.listdir(image_dir)
 os.chdir(image_dir)
@@ -22,3 +21,18 @@ for file in file_list:
         xpos = xypos.group(1)
         ypos = xypos.group(2)
         new_name = 'x' + xpos + '_y' + ypos + '.tif'
+
+# this script below walks through all subdirectories and will look for only tifs
+# still need something to confirm or be a little more careful
+# need to test that the script runs in a given dir as opposed to where the script lives
+
+
+def main():
+    path = os.getcwd()
+    count = 1
+
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if '.tif' in file:
+                print(os.path.join(root, file))
+
