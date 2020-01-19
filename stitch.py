085cd5a06f9401502595df12c626ee986caa9ce3
Nate Glasgow <ngglasgow@gmail.com>
2020-01-19 12:13:19 -0500

    made function that pulls out rows and cols

--- stitch.py
+++ stitch.py
@@ -1,29 +1,38 @@
 import imagej
 import numpy as np
-from skimage import io
 import os
+import re
 
-# set paths
-machine = os.uname()[0]
-if machine == 'Darwin':
-    home_dir = '/Volumes/Urban'
 
-elif machine == 'Linux':
-    home_dir = '/run/user/1000/gvfs/smb-share:server=sds1.neurobio.pitt.edu,share=urban'
+def server():
+    '''function to define path to server base directory'''
 
-else:
-    home_dir = os.path.join('Z:', os.sep)
+    machine = os.uname()[0]
+    if machine == 'Darwin':
+        server_dir = '/Volumes/Urban'
 
-project_dir = os.path.join(home_dir, 'Glasgow', 'ng_testing')
-figure_dir = os.path.join(project_dir, 'figures')
-table_dir = os.path.join(project_dir, 'tables')
-data_dir = os.path.join(project_dir, '190821')
-filedir_list = os.listdir(data_dir)
+    elif machine == 'Linux':
+        server_dir = '/run/user/1000/gvfs/smb-share:server=sds1.neurobio.pitt.edu,share=urban'
 
-imagej_version = 'sc.fiji:fiji:2.0.0-pre-8'
-fiji_path = os.path.join(home_dir, 'Fiji.app')
+    else:
+        server_dir = os.path.join('Z:', os.sep)
 
-ij = imagej.init(fiji_path, headless=False)
+    return server_dir
+
+
+home_path = os.path.expanduser('~')
+
+project_path = os.path.join(home_path, 'urban', 'mm_imaging')
+figure_path = os.path.join(project_path, 'figures')
+table_path = os.path.join(project_path, 'tables')
+data_path = os.path.join(project_path, '191014VJ_C448L')
+test_data_path = os.path.join(project_path, '191014VJ_C448L_4_1_gfp_1')
+
+# choose imagej/fiji to use, uncomment imagej_version to use the cached version
+# imagej_version = 'sc.fiji:fiji:2.0.0-pre-8'
+local_fiji = os.path.join(home_path, 'Fiji.app')
+
+ij = imagej.init(local_fiji, headless=False)
 
 ij.ui().showUI()
 
@@ -31,11 +40,15 @@ from jnius import autoclass
 IJ = autoclass('ij.IJ')
 # below is the JS output from fiji when running a fusion directly, need to put into python imagej format and go from there
 
+#TODO
+# need to figure out how to input variables into the text here easily
+# at minimum need x, y, and directory
+# maybe just make a plus script
 IJ.run("Grid/Collection stitching",
        "type=[Filename defined position] \
        order=[Defined by filename         ] \
-       grid_size_x=2 \
-       grid_size_y=6 \
+       grid_size_x={} \
+       grid_size_y={} \
        tile_overlap=20 \
        first_file_index_x=0 \
        first_file_index_y=0 \
@@ -47,7 +60,42 @@ IJ.run("Grid/Collection stitching",
        max/avg_displacement_threshold=2.50 \
        absolute_displacement_threshold=3.50 \
        compute_overlap computation_parameters=[Save memory (but be slower)] \
-       image_output=[Fuse and display]");
+       image_output=[Fuse and display]".format(x, y))
 imp = IJ.getImage()
 imp2 = imp.duplicate()
 imp.close()
+
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if '.tif' in file:
+                if 'Pos' in file:
+                    name_split = file.split('_')
+                    xpos = name_split[-2][-3:]
+                    ypos = name_split[-1][:3]
+
+                    new_name = 'x' + xpos + '_y' + ypos + '.tif'
+
+                    src = os.path.join(root, file)
+                    dst = os.path.join(root, new_name)
+
+                    os.rename(src, dst)
+
+def get_rows_columns(data_dir):
+    xs = []
+    ys = []
+    file_list = os.listdir(data_dir)
+    file_list.sort()
+    for file in file_list:
+        if '.tif' in file:
+            xypos = re.search('x(\d{3})_y(\d{3})', file)
+            xpos = int(xypos.group(1))
+            ypos = int(xypos.group(2))
+            xs.append(xpos)
+            ys.append(ypos)
+    xmax = np.array(xs).max()
+    ymax = np.array(ys).max()
+    return xmax, ymax
+
+
+x, y = get_rows_columns(test_data_path)
+
